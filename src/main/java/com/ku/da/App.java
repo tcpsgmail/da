4728f8e774fd84e335eb63c61d829a7301498268
A-Lisak <alex.lisak@esure.com>
2017-10-13 13:44:50 +0100

    uu

---
+++ src/main/java/com/ku/da/App.java
@@ -0,0 +1,13 @@
+package com.ku.da;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!aaa" );
+    }
+}
